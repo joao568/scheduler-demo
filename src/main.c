c50eeffc033e2c54d0a3aa700fb5c17ced2632a4
joao568 <jguilherme.marcos@gmail.com>
2017-10-02 21:24:11 -0300

    programa funcionando

--- src/main.c
+++ src/main.c
@@ -2,12 +2,23 @@
 
 int main() {
   int a[5];
-  int n;
+  int n, i, j, ind=0;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
+  for (i=1; i<=n; i++) {
+      for(j=0; j<=4; j++){
+            if(i%a[j]==0){
+                printf("%d", j);
+                ind=1;
+            }
+      }
+      if(ind==0){
+        printf("-");
+      }
+      printf("\n");
+      ind=0;
+
   }
 
   return 0;
